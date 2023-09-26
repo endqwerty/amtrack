4c8feedf5bf558319113f1e0c489f828be9ffed0
Daniel Yang <daniel@endqwerty.com>
2023-09-25 20:13:30 -0700

    working test using error

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     },
     baseUrl: 'https://www.amtrak.com',
     userAgent:
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
+      'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
     viewportHeight: 900,
     viewportWidth: 1440,
   },
--- cypress/e2e/amtrak.cy.js
+++ cypress/e2e/amtrak.cy.js
@@ -1,9 +1,34 @@
+const { DateTime } = require('luxon')
+
 describe('template spec', function () {
-  before(function () {
-    cy.visit('/')
+  beforeEach(function () {
+    cy.visit('/home.html', {
+      headers: {
+        'user-agent':
+          'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36',
+      },
+    })
     cy.findByRole('button', { name: 'Allow All' }).click()
+    cy.findByRole('button', { name: 'close button' }).click()
+    cy.url().should('contain', '/home.html')
   })
   it('passes', function () {
-    cy.findByRole('combobox', { name: 'From' })
+    const today = DateTime.now().plus({ days: 3 })
+    const threeDaysFromNow = today.plus({ days: 3 })
+    cy.configureCypressTestingLibrary({ testIdAttribute: 'amt-auto-test-id' })
+    cy.findAllByTestId('fare-finder-from-station-field-page').type('NYP')
+    cy.findAllByTestId('fare-finder-to-station-field-page').type('WAS')
+    cy.findByRole('textbox', { name: 'Depart Date' }).click()
+    cy.findByTestId('fare-finder-depart-date-oneway').type(
+      today.toLocaleString()
+    )
+    cy.findByRole('button', { name: 'Done' }).click()
+    cy.findByRole('button', { name: 'Return Date' }).click()
+    cy.findByRole('textbox', { name: 'Return Date' }).type(
+      threeDaysFromNow.toLocaleString()
+    )
+    cy.findByRole('button', { name: 'Done' }).click()
+    cy.findByRole('button', { name: 'FIND TRAINS' }).click()
+    cy.contains("We've experienced an unknown error.").should('be.visible')
   })
 })
